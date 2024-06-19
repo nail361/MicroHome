567eb6f6242ba6603a038f5dfde82f5affbc07e4
nail361 <nail361@mail.ru>
2024-06-19 17:21:10 +0300

    some changes to test CI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ToDo from './components/ToDo.jsx';
 
 const Header = lazy(() => import('MicroApp/Header'));
 
-// some changes to test CI 
+// some changes to test CI 2
 
 const App = () => {
   return (
