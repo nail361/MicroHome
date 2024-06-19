36575c15ad6191f67daa6f0532b402f9e2b9d57a
nail361 <nail361@mail.ru>
2024-06-19 16:54:08 +0300

    change import register

--- src/components/ToDoItems.jsx
+++ src/components/ToDoItems.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useStore } from '../store/mainStore';
 import { observer } from 'mobx-react-lite';
-import ToDoItem from './TodoItem';
+import ToDoItem from './ToDoItem';
 
 const ToDoItems = observer(({ todo }) => {
   const store = useStore();
